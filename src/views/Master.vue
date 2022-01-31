f35b662a786b73f180cc3b444cdf4c7fc94865a0
EngineerMithu <engineermithubd@gmail.com>
2022-01-31 14:13:30 +0600

    Vue Project Upload

---
+++ src/views/Master.vue
@@ -0,0 +1,20 @@
+<template>
+ <header-section></header-section>
+ <router-view></router-view>
+ <leter-section></leter-section>
+ <footer-section></footer-section>
+</template>
+
+<script>
+import HeaderSection from '../components/frontend/Header.vue';
+import LeterSection from '../components/frontend/Newsletter.vue';
+import FooterSection from '../components/frontend/Footer.vue';
+export default {
+    name: "Master",
+    components: { HeaderSection,LeterSection,FooterSection},
+};
+</script>
+
+<style lang="">
+    
+</style>
